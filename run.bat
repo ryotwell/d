71ca1faecd71249c077f7f827815ad1dcc422c40
Ryo Otwell <73240016+ryotwell@users.noreply.github.com>
2024-11-06 12:58:44 +0700

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,15 @@
+@echo off
+
+start cmd /k "pip install -r requirements.txt
+start cmd /k "python run.py"
+start cmd /k "python run.py"
+start cmd /k "python run.py"
+start cmd /k "python run.py"
+start cmd /k "python run.py"
+start cmd /k "python run.py"
+start cmd /k "python run.py"
+start cmd /k "python run.py"
+start cmd /k "python run.py"
+start cmd /k "python run.py"
+start cmd /k "python run.py"
+start cmd /k "python run.py"
